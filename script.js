37882ee57bc5e23c0915365b0f1f64c07dce8872
Salman Saleem <salmansaleempiaic@gmail.com>
2022-02-06 15:01:31 +0500

    dynamically changing color of the h1 first by reading h1 and then creating spans and then adding colors to them the for loop with same variable i needs further study

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const H1 = document.getElementById("h1");
-const COLORS = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];let color = '#';
+const counter = H1.textContent.length - 1;
+const COLORS = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
+const generatedColors = [];
+let color = '#';
 
 const getRandomColor = () => {
-    let color = '#';
+    let startC = '#';
     for(i=0;i<=5;i++){
         const index = COLORS[Math.floor(Math.random() * COLORS.length)];
-        color = color.concat(index)
+        startC = startC.concat(index)
     }
-    return color;
+    return startC;
 }
 
-
 const styleH1 = () => {
-    const h1 = document.createElement("h1")
-    const counter = H1.textContent.length;
     const charArray = H1.textContent.split("")
     console.log(counter);
     for(i=0;i<=counter;i++){
-        const span = document.createElement("span")
-        span.textContent = charArray[i]
-        window.document.body.appendChild(h1)
-        h1.appendChild(span)
+        const spanEl = document.createElement("span")
+        spanEl.textContent = charArray[i]
+        H1.appendChild(spanEl);
     }
+    H1.childNodes[0].remove()
+    for(z=0;z<=counter;z++){
+        // console.log(i);
+        // let y = getRandomColor();
+        // generatedColors.push(y);
+        H1.childNodes[z].style.color = getRandomColor();
+        console.log(H1.childNodes[z]);
+    }
+    console.log(generatedColors)
 }
+
+// Don't Change the above code
+
 
 // Onload
 styleH1()
